0f20bc86a26dc9b39dad5ad7afc5aa0dd27f9c80
Trevor Dawe <trevor.dawe@dell.com>
2021-02-05 10:46:20 -0500

    Added support for watching configuration changes

--- cmd/topology/main.go
+++ cmd/topology/main.go
@@ -11,6 +11,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -18,41 +19,63 @@ import (
 	"github.com/dell/karavi-topology/internal/entrypoint"
 	"github.com/dell/karavi-topology/internal/k8s"
 	"github.com/dell/karavi-topology/internal/service"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
 const (
-	port            = "443"
-	defaultCertFile = "/certs/localhost.crt"
-	defaultKeyFile  = "/certs/localhost.key"
+	port              = "443"
+	defaultCertFile   = "/certs/localhost.crt"
+	defaultKeyFile    = "/certs/localhost.key"
+	defaultConfigFile = "/etc/config/karavi-topology.yaml"
 )
 
 func main() {
 
-	provisionerNamesValue := os.Getenv("PROVISIONER_NAMES")
-	if provisionerNamesValue == "" {
-		fmt.Fprintf(os.Stderr, "PROVISIONER_NAMES is required")
-		os.Exit(1)
+	// enable viper to get properties from environment variables or default configuration file
+	viper.AutomaticEnv()
+	viper.SetConfigFile(defaultConfigFile)
+
+	err := viper.ReadInConfig()
+	// if unable to read configuration file, proceed in case we use environment variables
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to read config file: %v", err)
 	}
 
-	provisionerNames := strings.Split(provisionerNamesValue, ",")
-
-	volumeFinder := k8s.VolumeFinder{
-		API:         &k8s.API{},
-		DriverNames: provisionerNames,
+	volumeFinder := &k8s.VolumeFinder{
+		API: &k8s.API{},
 	}
 
-	certFile := os.Getenv("TLS_CERT_PATH")
+	updateDriverNames := func(volumeFinder *k8s.VolumeFinder) {
+		provisionerNamesValue := viper.GetString("PROVISIONER_NAMES")
+		provisionerNames := strings.Split(provisionerNamesValue, ",")
+		volumeFinder.DriverNames = provisionerNames
+		log.Printf("Set DriverNames to %s", provisionerNames)
+	}
+
+	updateDriverNames(volumeFinder)
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Printf("Config file changed: %v", e)
+		updateDriverNames(volumeFinder)
+	})
+
+	// TLS_CERT_PATH is only read as an environment variable
+	certFile := viper.GetString("TLS_CERT_PATH")
 	if len(strings.TrimSpace(certFile)) < 1 {
 		certFile = defaultCertFile
 	}
 
-	keyFile := os.Getenv("TLS_KEY_PATH")
+	// TLS_KEY_PATH is only read as an environment variable
+	keyFile := viper.GetString("TLS_KEY_PATH")
 	if len(strings.TrimSpace(keyFile)) < 1 {
 		keyFile = defaultKeyFile
 	}
 
 	var bindPort int
-	portEnv := os.Getenv("PORT")
+	// PORT is only read as an environment variable
+	portEnv := viper.GetString("PORT")
 	if portEnv != "" {
 		var err error
 		if bindPort, err = strconv.Atoi(portEnv); err != nil {
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/dell/karavi-topology
 go 1.14
 
 require (
-	github.com/golang/mock v1.2.0
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/golang/mock v1.3.1
 	github.com/gorilla/mux v1.8.0
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.4.0
 	k8s.io/api v0.17.3
 	k8s.io/apimachinery v0.17.3
